ded9136f628e73d500a36db17a8474fe015af283
sunjianxu <13531052984@163.com>
2021-04-08 15:05:22 +0800

    :wrench: 修改配置

--- vue.config.js
+++ vue.config.js
@@ -9,7 +9,7 @@ module.exports = {
       entry: 'src/main.js', // page 的入口
       template: 'src/public/index.html', // 模板来源
       filename: 'index.html', // 在 dist/index.html 的输出
-      title: process.env.VUE_GAME_PACKAGE_NAME, // 当使用 title 选项时， template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
+      title: process.env.VUE_APP_TITLE, // 当使用 title 选项时， template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
       chunks: ['chunk-vendors', 'chunk-common', 'index'] // 在这个页面中包含的块，默认情况下会包含 提取出来的通用 chunk 和 vendor chunk。
     }
   },
